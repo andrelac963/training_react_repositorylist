24b35b2d6b9b9d432cdf47d164ca009a593e6415
André Luiz Alves Costa <andrelac963@gmail.com>
2022-02-26 17:29:50 -0300

    Dev: iniciando projeto em TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { RepositoryItem } from './components/RepositoryItem'
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com'
-}
-
-import './styles/global.css'
+import './styles/global.scss'
 
 function App() {
   return (
-    <section className="repository-list">
-      <h1>Lista de Repositórios</h1>
-      <ul>
-        <RepositoryItem
-          name={repository.name}
-          description={repository.description}
-          link={repository.link}
-        />
-        <RepositoryItem
-          name={repository.name}
-          description={repository.description}
-          link={repository.link}
-        />
-        <RepositoryItem
-          name={repository.name}
-          description={repository.description}
-          link={repository.link}
-        />
-      </ul>
-    </section>
+    <div>
+      <p>olá</p>
+    </div>
   )
 }
 
